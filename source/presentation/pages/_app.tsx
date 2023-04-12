5ab19d800726a6f6d196cdc0b9d3a8fef3f138d7
Gabriel Rodrigues <gabriel.bolonha@novahaus.com.br>
2023-04-12 18:24:17 -0300

    feat: Add swiper to dependencies, creating images folder
    Add swiper to dependencies to use on carousel component and creating images folder with logo images

--- source/presentation/pages/_app.tsx
+++ source/presentation/pages/_app.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
 import "@/presentation/styles/globals.css";
 
+const poppins = Poppins({
+  weight: "400",
+  variable: "--poppins",
+});
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --poppins: ${poppins.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />{" "}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,19 @@ module.exports = {
     './source/presentation/pages/**/*.{js,ts,jsx,tsx}',
     './source/presentation/components/**/*.{js,ts,jsx,tsx}',
   ],
-  mode: 'jit',
   theme: {
-    extend: {},
+    fontFamily:{
+      poppins: ["var(--poppins)"]
+    },
+    extend: {
+      colors:{
+        darkGreen: "#004238",
+        lightGreen: "#3DCCB7",
+        mediumGreen: "#00C3A5",
+        lightBg: "#F7F9F9",
+        mediumBg: "#D6F8F3"
+      } 
+    },
   },
   variants: {
     extend: {},
